1699c36319736b6a3a71ac90354e10e3bd174b70
CodeWithGamez <codewithgamez@gmail.com>
2020-08-16 21:01:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#Fix my mistakes of using VARIABLES AND TYPES
+
+
+#1 I want this to print hello
+print(greetings)
+greetings = "hello"
+
+
+
+
+
+#2 I want this to print the number 4
+number = 4
+print(NUMBER)
+
+
+
+
+
+
+#3 I want this to print out my words
+words = "learning python"
+
+
+
+
+
+
+#4 I want this to print the types of string, int, float instead of two, 2 , 2.0
+print("two")
+print(2)
+print(2.0)
+
+
+
+
+
+
+#5 I want the types printed to the console
+type("practice")
+type(7)
+type(43312.456465132)
+
